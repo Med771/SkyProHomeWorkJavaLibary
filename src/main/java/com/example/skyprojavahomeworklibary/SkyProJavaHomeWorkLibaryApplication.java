4320e09654ad1f9fdaf3d67e9b8cb78a5b3facdc
Med771 <mm7737333@gmail.com>
2024-11-07 14:42:31 +0300

    Begin

---
+++ src/main/java/com/example/skyprojavahomeworklibary/SkyProJavaHomeWorkLibaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprojavahomeworklibary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProJavaHomeWorkLibaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProJavaHomeWorkLibaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprojavahomeworklibary/SkyProJavaHomeWorkLibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprojavahomeworklibary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProJavaHomeWorkLibaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
